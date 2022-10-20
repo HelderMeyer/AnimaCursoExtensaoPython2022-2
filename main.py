bfef2eda8ac6e660c29595a9c2aaf30f8432fb64
Helder Meyer <60283346+HelderMeyer@users.noreply.github.com>
2022-10-20 00:55:19 +0000

    Aprendi a criar variáveis e mostrá-las na tela

--- main.py
+++ main.py
@@ -4,6 +4,18 @@
 
 print("Hello World!")
 
+# Quando quiser guardar uma String! (frase)
+
+nome = "Helder Meyer"
+
+# Quando quiser guardar um número inteiro
+
+idade = 19
+
+# Exibir o meu nome (que está dentro da variável nome)
+
+print(nome)
+
 '''
 Comentário em bloco
 '''
