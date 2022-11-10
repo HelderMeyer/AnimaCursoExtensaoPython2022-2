f5a614f7839243fcd9f4599c4e559ba6dce54ebe
Helder Meyer <60283346+HelderMeyer@users.noreply.github.com>
2022-11-10 01:03:01 +0000

    Usando a função no forEach

--- aula3b-2022-11-09.py
+++ aula3b-2022-11-09.py
@@ -22,3 +22,13 @@ def calcular_imposto(preco_produto):
 imposto3 = calcular_imposto(preco3)
 
 print("O valor do imposto é de R${}".format(imposto3))
+
+# Agora calcular imposto a alíquota agora é 7%
+
+valores = [1.99, 24.50, 78.27, 1515.5]
+
+# Se eu quiser calcular o imposto destes quatro valores... e exibir na tela assim: "O imposto de ... é ..." (1o. preço, 2o. imposto)
+
+for valor in valores:
+
+  print(f"O imposto de {valor} é {calcular_imposto(valor)}")
